9d27bb5c1adbf5fb30957dd4952007c12a9cec0c
saysa <saysabou@gmail.com>
2015-06-22 08:39:32 +0200

    Debug fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,10 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Vyper\SiteBundle\VyperSiteBundle(),
+            new Vyper\UserBundle\VyperUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Genemu\Bundle\FormBundle\GenemuFormBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
