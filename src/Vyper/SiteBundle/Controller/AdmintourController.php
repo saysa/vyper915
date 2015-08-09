a262d4aee8020442262a414462a922895235d27e
saysa <saysabou@gmail.com>
2015-08-09 16:00:31 +0200

    Admin panel : Tours - roles restricted

--- src/Vyper/SiteBundle/Controller/AdmintourController.php
+++ src/Vyper/SiteBundle/Controller/AdmintourController.php
@@ -15,6 +15,11 @@ use Vyper\SiteBundle\Form\TourType;
 
 class AdminTourController extends AdminCommonController {
 
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Security("has_role('ROLE_ADMIN')")
+     */
     public function addTourAction(Request $request)
     {
         $view = $this->container->get('saysa_view');
@@ -48,7 +53,7 @@ class AdminTourController extends AdminCommonController {
      * @param Request $request
      * @param Tour $tour
      * @return \Symfony\Component\HttpFoundation\Response
-     * @Security("has_role('ROLE_AUTHOR')")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function updateTourAction(Request $request, Tour $tour)
     {
@@ -76,6 +81,11 @@ class AdminTourController extends AdminCommonController {
         return $this->render('VyperSiteBundle:Admintour:updateTour.html.twig', $view->getView());
     }
 
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Security("has_role('ROLE_ADMIN')")
+     */
     public function showToursAction(Request $request)
     {
         $view = $this->container->get('saysa_view');
