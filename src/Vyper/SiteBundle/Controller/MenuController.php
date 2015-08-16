e97aee1f4fa511c76e129782afd1fcfda5cddcca
saysa <saysabou@gmail.com>
2015-08-16 16:39:19 +0200

    Front : Menu - Show Big Highlight Article in Articles menu

--- src/Vyper/SiteBundle/Controller/MenuController.php
+++ src/Vyper/SiteBundle/Controller/MenuController.php
@@ -4,19 +4,25 @@ namespace Vyper\SiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class MenuController extends Controller
 {
 
-    public function MenuAction()
+    public function MenuAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
+        $locale = $em->getRepository('VyperSiteBundle:LocaleType')->findByName($request->getLocale());
 
+        $articles_carousel = $em->getRepository('VyperSiteBundle:Article')->carousel($locale);
         $themesInMenu = $em->getRepository('VyperSiteBundle:Theme')->getShowInMenu();
 
         $view = $this->container->get('saysa_view');
-        $view->set('themes_in_menu', $themesInMenu);
+        $view
+            ->set('themes_in_menu', $themesInMenu)
+            ->set('highlight_in_menu', $articles_carousel)
+        ;
 
         return $this->render('VyperSiteBundle:Menu:Menu.html.twig', $view->getView());
     }
