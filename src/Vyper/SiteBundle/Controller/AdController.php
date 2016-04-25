e7c2c4f671497090c99d59affd63a9cf9af527f9
Saysa Bounkhong <saysa.bounkhong@digitalmobil.com>
2016-04-25 19:26:25 +0200

    Progress on Ad header : Ad header slot for mobile

--- src/Vyper/SiteBundle/Controller/AdController.php
+++ src/Vyper/SiteBundle/Controller/AdController.php
@@ -25,4 +25,21 @@ class AdController extends Controller
 
         return $this->render('VyperSiteBundle:Ad:Header468.html.twig', $view->getView());
     }
+
+    public function Header320Action(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $locale = $em->getRepository('VyperSiteBundle:LocaleType')->findByName($request->getLocale());
+
+        $view = $this->container->get('saysa_view');
+
+        $adType = $em->getRepository('VyperSiteBundle:AdType')->findByName('Header (320x100px)');
+        $header = $em->getRepository('VyperSiteBundle:Ad')->getSquare($locale, $adType);
+
+        $view
+            ->set('ad_header', $header)
+        ;
+
+        return $this->render('VyperSiteBundle:Ad:Header320.html.twig', $view->getView());
+    }
 }
