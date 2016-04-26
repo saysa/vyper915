0ab35f02e407590e48306e7e6fa9bc3ca9f5161f
Saysa Bounkhong <saysa.bounkhong@digitalmobil.com>
2016-04-26 09:45:20 +0200

    Progress on Ad header : add header slot for desktop large 728px

--- src/Vyper/SiteBundle/Controller/AdController.php
+++ src/Vyper/SiteBundle/Controller/AdController.php
@@ -5,10 +5,38 @@ namespace Vyper\SiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-
+/**
+ * Class AdController
+ *
+ * @package Vyper\SiteBundle\Controller
+ */
 class AdController extends Controller
 {
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function Header728Action(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $locale = $em->getRepository('VyperSiteBundle:LocaleType')->findByName($request->getLocale());
 
+        $view = $this->container->get('saysa_view');
+
+        $adType = $em->getRepository('VyperSiteBundle:AdType')->findByName('Header (728x90px)');
+        $header = $em->getRepository('VyperSiteBundle:Ad')->getSquare($locale, $adType);
+
+        $view
+            ->set('ad_header', $header)
+        ;
+
+        return $this->render('VyperSiteBundle:Ad:Header728.html.twig', $view->getView());
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function Header468Action(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
@@ -26,6 +54,10 @@ class AdController extends Controller
         return $this->render('VyperSiteBundle:Ad:Header468.html.twig', $view->getView());
     }
 
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function Header320Action(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
